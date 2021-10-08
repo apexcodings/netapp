8032edb64623e594e2b22c24224fa735569f9d55
dreamerdad <hugh@vmrcommunications.com>
2021-10-08 19:24:01 -0400

    Update vmr-crux-gpt.js

--- src/crux/js/vmr-crux-gpt.js
+++ src/crux/js/vmr-crux-gpt.js
@@ -1,7 +1,7 @@
 // Ad slots will be lazy-loaded with the config below
 // Ad slots will be refreshed if viewable
 
-// CONFIGURATION BELOW-TEST
+// CONFIGURATION BELOW-TEST-123
 
 // Number of seconds a slot should be viewable before it refreshes
 const SECONDS_TO_WAIT_AFTER_VIEWABILITY = 30;
