a003472f2bbc2cdef676252ccfdfb791b921c5f8
dreamerdad <hugh@vmrcommunications.com>
2021-10-18 17:17:50 -0400

    Update vmr-desales-gpt.js

--- src/desales/js/vmr-desales-gpt.js
+++ src/desales/js/vmr-desales-gpt.js
@@ -2,11 +2,13 @@
 window.googletag = window.googletag || {cmd: []};
   googletag.cmd.push(function() {
     googletag.defineSlot('/270537003/TheTabletOtherFooter728', [728, 90], 'div-gpt-ad-1432670765502-0').addService(googletag.pubads());
-    googletag.defineSlot('/270537003/thetabletsidebartopinsidepage', [300, 250], 'div-gpt-ad-1432670765502-1').addService(googletag.pubads());
-    googletag.defineSlot('/270537003/thetabletsidebarmiddleinsidepage', [300, 250], 'div-gpt-ad-1432670765502-2').addService(googletag.pubads());
-    googletag.defineSlot('/270537003/thetabletsidebarbottominsidepage', [300, 250], 'div-gpt-ad-1432670765502-3').addService(googletag.pubads());
-    googletag.defineSlot('/270537003/TheTablet_Sidebar_300x250_InsidePage', [300, 250], 'div-gpt-ad-1432670765502-4').addService(googletag.pubads());
+    googletag.defineSlot('/270537003/thetabletsidebartopinsidepage', [300, 250], 'div-gpt-ad-1432670765502-4').addService(googletag.pubads());
+    googletag.defineSlot('/270537003/thetabletsidebarmiddleinsidepage', [300, 250], 'div-gpt-ad-1432670765502-1').addService(googletag.pubads());
+    googletag.defineSlot('/270537003/thetabletsidebarbottominsidepage', [300, 250], 'div-gpt-ad-1432670765502-2').addService(googletag.pubads());
+    googletag.defineSlot('/270537003/TheTablet_Sidebar_300x250_InsidePage', [300, 250], 'div-gpt-ad-1432670765502-3').addService(googletag.pubads());
     googletag.defineSlot('/270537003/tablet-300x600', [300, 600], 'div-gpt-ad-1473119081787-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
   });
+
+
